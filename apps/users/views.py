5b2eb4cecbde85927f9480b336211c88e0586592
abdullaabdukulov <abduqulovabdulla3108@gmail.com>
2023-11-27 23:11:49 +0500

    fixed bugs

--- apps/users/views.py
+++ apps/users/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def user_main(request):
+    return render(request, 'users/user_main.html')
--- config/urls.py
+++ config/urls.py
@@ -14,26 +14,28 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 
-from config.settings import django_settings_module
+from config.settings import (
+    base as settings,
+    django_settings_module,
+)
 
 urlpatterns = [path("default-admin-panel/", admin.site.urls)]
 
 urlpatterns += [
-    path("users/", include(("users.urls", "users"), "users")),
+    path("", include(("users.urls", "users"), "users")),
 ]
 
 if django_settings_module == "development":
     urlpatterns += [
         path("__debug__/", include("debug_toolbar.urls")),
     ]
-    urlpatterns += static(
-        settings.STATIC_URL, document_root=settings.STATIC_ROOT
-    )
+    # urlpatterns += static(
+    #     settings.STATIC_URL, document_root=settings.STATIC_ROOT
+    # )
     urlpatterns += static(
         settings.MEDIA_URL, document_root=settings.MEDIA_ROOT
     )
